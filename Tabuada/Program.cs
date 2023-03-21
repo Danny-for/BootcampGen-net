75e6c363f451ee214c30b51c62a265d5b018fba9
Danny-for <dannygeovanna2019@gmail.com>
2023-03-21 18:43:47 -0300

    Atividades

---
+++ Tabuada/Program.cs
@@ -0,0 +1,20 @@
+﻿using System;
+
+namespace Tabuada
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            int valor, num;
+            Console.WriteLine("Coloque o número para ter a tabuada : ");
+
+            valor = int.Parse(Console.ReadLine());
+
+            for (num = 1; num <= 10; num++)
+            {
+                Console.WriteLine($"{num} X {valor} = {num * valor}");
+            }
+        }
+    }
+}
